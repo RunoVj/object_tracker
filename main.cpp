f038a74efa34b67a43e24cd40019bdf14c8070e6
Ilya <ilya.litik@gmail.com>
2017-10-09 20:17:22 +0300

    Add SURF detector

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/video.hpp>
+
+#include <unistd.h>
 #include "harrisdetector.h"
 
 #define path1 "/home/ilya/Code/ComputerVisionProjects/Images/harris.png"
 #define path2 "/home/ilya/Code/ComputerVisionProjects/Images/ugol.jpg"
 
+using namespace cv::xfeatures2d;
 
-int main(int argc, char** argv )
+int harris_test(cv::VideoCapture cap)
 {
-    cv::VideoCapture cap(0);
-    if(!cap.isOpened()){
-        return -1;
-    }
     cv::Mat frame, edges, cornerStrength;
 
     HarrisDetector harris;
@@ -25,7 +28,7 @@ int main(int argc, char** argv )
         cv::cvtColor(frame, edges, cv::COLOR_BGR2GRAY);
         std::vector<cv::Point> pts;
 
-        cv::GaussianBlur(edges, edges, cv::Size(7,7), 1.5, 1.5);
+//        cv::GaussianBlur(edges, edges, cv::Size(3,3), 1.5, 1.5);
 //        cv::Canny(edges, edges, 0, 30, 3);
 
         harris.detect(edges);
@@ -39,7 +42,54 @@ int main(int argc, char** argv )
             break;
         }
     }
+    return 0;
+}
+
+
+int main(int argc, char** argv )
+{
+    cv::VideoCapture cap(0);
+    if(!cap.isOpened()){
+        return -1;
+    }
+
+    cv::Mat frame, gray_fr, edges, cornerStrength;
+    uint64 start;
+    double fps;
+
+    HarrisDetector harris;
+
+    for(;;){
+        start = cv::getTickCount();
+        std::vector<cv::Point> keypoints1;
+        std::vector<cv::KeyPoint> keypoints2;
+        cap >> frame;
+
+        cv::cvtColor(frame, gray_fr, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(gray_fr, gray_fr, cv::Size(5, 5), 1.5, 1.5);
+
+        harris.detect(gray_fr);
+        harris.getCorners(keypoints1, 0.01);
+
+        harris.drawOnImage(frame, keypoints1, cv::Scalar(255, 0, 0));
+
+
+        cv::Ptr<SURF> surf = SURF::create(2500.);
+        surf->detect(gray_fr, keypoints2);
+
+        cv::drawKeypoints(frame, keypoints2, frame, cv::Scalar(0, 255, 0), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        cv::imshow("edges", frame);
+
+        if(cv::waitKey(5) >= 0){
+            break;
+        }
+
+        fps = cv::getTickFrequency() / (cv::getTickCount() - start);
+        std::cout << "FPS : " << fps << std::endl;
+    }
 
     return 0;
 }
+
+
 
